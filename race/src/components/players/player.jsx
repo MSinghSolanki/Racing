0e1f7c29057c80d06d8c5db0682b35ab80bf417e
MSinghSolanki <112701441+MSinghSolanki@users.noreply.github.com>
2023-08-24 17:28:01 +0530

    proper logic

--- race/src/components/players/player.jsx
+++ race/src/components/players/player.jsx
@@ -1,110 +1,119 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './player.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHorse, faDog, faCat, faFish, faDove, faBug } from '@fortawesome/free-solid-svg-icons';
-import { RankModal } from '../ranks/rank';
-import emailjs from '@emailjs/browser';
-
-const animalIcons = [faHorse, faDog, faCat, faFish, faDove, faBug];
 
 export const Players = () => {
-  const [winner, setWinner] = useState(null);
+  const bikeCount = 4; // Number of bikes
+  const raceDistance = 1000; // Race distance in km
+  const raceDuration = 60; // Race duration in seconds (2 minutes)
+
+  const [bikes, setBikes] = useState([]);
+  const [intervalId, setIntervalId] = useState(null); // New state for interval ID
   const [raceInProgress, setRaceInProgress] = useState(false);
-  const [rankModalOpen, setRankModalOpen] = useState(false);
-  const [ranks, setRanks] = useState([]);
-  const [speeds, setSpeeds] = useState([]);
+  const [showSpeeds, setShowSpeeds] = useState(false);
+  const [winner, setWinner] = useState(null);
+  const [showFollowUp, setShowFollowUp] = useState(false);
 
-  const openRankModal = () => {
-    setRankModalOpen(true);
-  };
+  useEffect(() => {
+    // Generate initial bike data
+    const initialBikes = generateInitialBikes();
+    setBikes(initialBikes);
 
-  const closeRankModal = () => {
-    setRankModalOpen(false);
-  };
+    // Clean up interval on unmount
+    return () => clearInterval(intervalId);
+  }, []); // No need to start the interval here
 
-  const sendEmail = () => {
-    emailjs.send(
-      'service_nk4vm0d', // Your email service ID from emailjs dashboard
-      'template_63eg5is', // Your template ID from emailjs dashboard
-      {
-        to_email: 'mohit.solanki@allen.in', // Replace with recipient's email
-      },
-      'V32r9vN3iCxD_q1ka' // Your user ID from emailjs dashboard
-    )
-    .then((response) => {
-      console.log('Email sent successfully', response);
-    })
-    .catch((error) => {
-      console.error('Error sending email', error);
-    });
-  };
-
-  const handleTransitionEnd = () => {
-    // Check if all animal animations have finished
-    const animalElements = document.querySelectorAll('.animal');
-    const unfinishedAnimations = Array.from(animalElements).filter(
-      (animal) => animal.style.left !== `${window.innerWidth - 100}px`
-    );
-
-    if (unfinishedAnimations.length === 0) {
-      const sortedRanks = speeds.map((speed, index) => ({ speed, icon: animalIcons[index] }))
-        .sort((a, b) => a.speed - b.speed);
-      setRanks(sortedRanks);
-      sendEmail();
-      setTimeout(openRankModal, 1000);
+  const generateInitialBikes = () => {
+    const newBikes = [];
+    for (let i = 0; i < bikeCount; i++) {
+      newBikes.push({
+        id: i + 1,
+        name: `Bike ${i + 1}`,
+        speed: 0,
+        distance: 0,
+      });
     }
+    return newBikes;
   };
 
   const startRace = () => {
     if (!raceInProgress) {
+      // Start the race
+      const id = setInterval(updateBikeSpeeds, 1000); // Update speeds every 1 second
+      setIntervalId(id);
       setRaceInProgress(true);
-      setWinner(null);
-
-      const endPoint = window.innerWidth - 100; // Adjust based on icon width
-      const newSpeeds = Array.from({ length: 6 }, () => Math.random() * 5 + 1);
-      setSpeeds(newSpeeds);
-
-      const animalElements = document.querySelectorAll('.animal');
-
-      animalElements.forEach((animal, index) => {
-        const speed = newSpeeds[index];
-        const animationDuration = endPoint / speed;
-
-        animal.style.transition = `left ${animationDuration}s linear`;
-        animal.style.left = endPoint + 'px';
-
-        animal.addEventListener('transitionend', () => {
-          if (animal.style.left === endPoint + 'px' && !winner) {
-            setWinner(index);
-            setRaceInProgress(false);
-          }
-        });
-      });
     }
   };
 
+  const updateBikeSpeeds = () => {
+    setBikes((prevBikes) =>
+      prevBikes.map((bike) => {
+        if (bike.distance >= raceDistance && !raceInProgress) {
+          console.log(`Bike ${bike.id} has won!`);
+          console.log(`Simulating email to Bike ${bike.id} winner.`);
+          setWinner(bike.id);
+          setShowFollowUp(true);
+          setRaceInProgress(true);
+        }
+        return {
+          ...bike,
+          speed: Math.random() * 5 + 1,
+          distance: bike.distance + bike.speed,
+          percentage: (bike.distance + bike.speed) / raceDistance * 100,
+        };
+      })
+    );
+  };
+
+  const toggleSpeeds = () => {
+    setShowSpeeds(!showSpeeds);
+  };
+
+  const resetRace = () => {
+    setWinner(null);
+    setShowFollowUp(false);
+    setBikes(generateInitialBikes());
+    setRaceInProgress(false);
+  };
 
   return (
-    <div>
-        <h1>Get Ready!!!!!</h1>
-        <h1>For The Show</h1>
-    <div className="players">
+    <div className="race-container">
       <div className="race-track">
-        {animalIcons.map((icon, index) => (
-          <div className={`animal ${index === winner ? 'winner' : ''}`} key={index}>
-            <FontAwesomeIcon icon={icon} />
-          </div>
-        ))}
+        <svg width={raceDistance} height="100">
+          {/* Race track */}
+          <rect x="0" y="0" width={raceDistance} height="100" fill="#ddd" />
+
+          {/* Bikes */}
+          {bikes.map((bike, index) => (
+            <rect
+              key={bike.id}
+              x="0"
+              y={index * 30}
+              width={`${bike.percentage}%`}
+              height="20"
+              fill={winner === bike.id ? '#f39c12' : `hsl(${120 - bike.percentage}, 100%, 50%)`} // Change color based on percentage
+            />
+          ))}
+        </svg>
       </div>
-       <button onClick={startRace} disabled={raceInProgress}>
+      <div className={`speed-popup ${showSpeeds ? 'visible' : ''}`}>
+        <h2>Bike Speeds</h2>
+        <ul>
+          {bikes.map((bike) => (
+            <li key={bike.id}>Bike {bike.id}: {bike.speed.toFixed(2)} km/h</li>
+          ))}
+        </ul>
+      </div>
+      <div className={`follow-up-popup ${showFollowUp ? 'visible' : ''}`}>
+        <h2>Race Completed!</h2>
+        <p>{winner ? `Bike ${winner} is the winner!` : 'No winner this time.'}</p>
+        <button onClick={resetRace}>Start Another Race</button>
+      </div>
+      <button className="race-button" onClick={startRace} disabled={raceInProgress}>
         {raceInProgress ? 'Race in Progress' : 'Start Race'}
       </button>
-      <p className="winner-text">{winner !== null && `Winner: ${animalIcons[winner].iconName}`}</p>
-
-<RankModal isOpen={rankModalOpen} onClose={closeRankModal} ranks={ranks} />
-</div>
-</div>
+      <button className="speed-button" onClick={toggleSpeeds}>
+        {showSpeeds ? 'Hide Speeds' : 'Show Speeds'}
+      </button>
+    </div>
   );
 };
-
-
